40b545b0b7ce30c9caa4cbdde72dc1e6be62e62b
John Crissman <JohnMCrissmanUSMC@gmail.com>
2019-11-14 16:57:19 -0600

    added Backtracking and CSP classes

---
+++ py/Backtracking.py
@@ -0,0 +1,12 @@
+class Backtracking:
+    def backtracking(self, csp):
+        return [1,2,3,4,5,6,7]
+
+    def MRV(self):
+        return 1
+
+    def degree(self):
+        return 2
+
+    def LCV(self):
+        return 3
---
+++ py/CSP.py
@@ -0,0 +1,6 @@
+class CSP:
+    def __init__(self, variables = [1,2,3], domains = [1,2,3], constraints = [1,2,3], solution = []):
+        self.variables = variables
+        self.domains = domains
+        self.constraints = constraints
+        self.solution = solution
