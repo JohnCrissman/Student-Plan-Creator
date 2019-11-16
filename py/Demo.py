630d8b74889590b028454f32f6ce856e8b86939f
Marianela Crissman <marianela_min@yahoo.com>
2019-11-16 11:47:42 -0600

    added gitignore so we dont have to commit .idea/ settings.

---
+++ py/Demo.py
@@ -0,0 +1,11 @@
+from py.Backtracking import Backtracking
+
+
+def main():
+    #    this main should use the classs created to generate the student's plan
+    #
+    print('hello from main')
+
+
+if __name__ == '__main__':
+    main()
