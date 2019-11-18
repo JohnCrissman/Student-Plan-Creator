c992b7ddc6ab1c369b516e875988ededade5471a
Nazanin Miryani <nazaninmiryani@Nazanins-MacBook-Pro.local>
2019-11-18 08:55:30 -0600

    Commiting student/j_demo/n_demo/n_info

--- py/Student.py
+++ py/Student.py
@@ -9,8 +9,30 @@ class Student:
         self.filename = filename
         self.list_of_classes = list_of_classes
 
+    @staticmethod
     def load(self, filename):
         print("This load file to student object")
 
+    def get_semester(self):
+        return self.semester
 
+    def set_semester(self):
+        return self.semester
 
+    def get_year(self):
+        return self.year
+
+    def set_year(self):
+        return self.year
+
+    def get_filename(self):
+        return self.filename
+
+    def set_filename(self):
+        return self.filename
+
+    def get_list_of_classes(self):
+        return self.list_of_classes
+
+    def set_list_of_classes(self):
+        return self.list_of_classes
