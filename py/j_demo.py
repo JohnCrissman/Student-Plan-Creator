45c392814316bed5da3fc20b7e900f777339b137
John Crissman <JohnMCrissmanUSMC@gmail.com>
2019-11-26 17:09:28 -0600

    created unit tests for CSP and CoursesOffered

--- py/j_demo.py
+++ py/j_demo.py
@@ -7,16 +7,49 @@ from py.CoursesOffered import CoursesOffered
 
 def main():
     print("\n====================================================== John Test\n")
-    filename = "j_info.txt"
+    filename_fall_2019 = "fall_2019.txt"
+    filename_fall_2008 = "fall_2008.txt"
+    filename_spring_2019 = "spring_2019.txt"
+    filename_spring_2008 = "spring_2008.txt"
+    filename_summer_2019 = "summer_2019.txt"
+    filename_summer_2008 = "summer_2008.txt"
 
-    student = Student(filename)
-    courses_offered = CoursesOffered(student)
-    csp = CSP(courses_offered)
+    student_fall_2019 = Student(filename_fall_2019)
+    student_fall_2008 = Student(filename_fall_2008)
+    student_spring_2019 = Student(filename_spring_2019)
+    student_spring_2008 = Student(filename_spring_2008)
+    student_summer_2019 = Student(filename_summer_2019)
+    student_summer_2008 = Student(filename_summer_2008)
 
-    print("here are the domains")
-    print(csp.domains)
-    bt = Backtracking(csp)
-    bt.backtracking_algorithm()
+
+    courses_offered_fall_2019 = CoursesOffered(student_fall_2019)
+    courses_offered_fall_2008 = CoursesOffered(student_fall_2008)
+    courses_offered_spring_2019 = CoursesOffered(student_spring_2019)
+    courses_offered_spring_2008 = CoursesOffered(student_spring_2008)
+    courses_offered_summer_2019 = CoursesOffered(student_summer_2019)
+    courses_offered_summer_2008 = CoursesOffered(student_summer_2008)
+
+    csp_fall_2019 = CSP(courses_offered_fall_2019)
+    csp_fall_2008 = CSP(courses_offered_fall_2008)
+    csp_spring_2019 = CSP(courses_offered_spring_2019)
+    csp_spring_2008 = CSP(courses_offered_spring_2008)
+    csp_summer_2019 = CSP(courses_offered_summer_2019)
+    csp_summer_2008 = CSP(courses_offered_summer_2008)
+
+    print("\nDomain for Fall 2019: \n", csp_fall_2019.domains)
+    print("\nDomain for Fall 2008: \n", csp_fall_2008.domains)
+    print("\nDomain for Spring 2019: \n", csp_spring_2019.domains)
+    print("\nDomain for Spring 2008: \n", csp_spring_2008.domains)
+    print("\nDomain for Summer 2019: \n", csp_summer_2019.domains)
+    print("\nDomain for Summer 2008: \n", csp_summer_2008.domains)
+
+
+    # csp = CSP(courses_offered)
+    #
+    # print("here are the domains")
+    # print(csp.domains)
+    # bt = Backtracking(csp)
+    # bt.backtracking_algorithm()
 
 if __name__ == '__main__':
     main()
---
+++ test/CSPTest.py
@@ -0,0 +1,147 @@
+import unittest
+from py.CSP import CSP
+from py.CoursesOffered import CoursesOffered
+from py.Student import Student
+
+
+class TestCSP(unittest.TestCase):
+
+    def test_should_return_true_for_consistency(self):
+        assignment = {'S1C1': 'CS400', 'S1C2': 'CS404', 'S2C3': 'CS335', 'S4C2': 'CS420', 'S4C3': 'CS490', 'S2C2': 'CS411', 'S2C1': 'CS331'}
+        candidate = "S3C2"
+        val = "419"
+        filename = "j_info.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        test_consistency = csp.is_assign_consistent(candidate, val, assignment)
+        self.assertEqual(True, test_consistency, "consistency test epic fail :D ")
+
+    def test_should_return_correct_domain_for_fall_2019(self):
+        filename = "fall_2019.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        domains = csp.domains
+        self.assertEqual({'S1C1': ['CS400'],
+                          'S1C2': ['CS404'],
+                          'S1C3': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS401', 'CS404', 'CS413', 'CS415',
+                                   'CS419', 'CS460', 'CS490'],
+                          'S2C1': ['CS331', 'CS355', 'CS442'],
+                          'S2C2': ['CS411', 'CS412'],
+                          'S2C3': ['CS335'],
+                          'S3C1': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S3C2': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S3C3': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S4C1': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS404', 'CS412', 'CS416', 'CS420',
+                                   'CS440', 'CS460', 'MATH305', 'ECON401', 'CS490'],
+                          'S4C2': ['CS420'],
+                          'S4C3': ['CS490']},
+                         domains, "test that should get the correct domain for fall 2019")
+
+    def test_should_return_correct_domain_for_fall_2008(self):
+        filename = "fall_2008.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        domains = csp.domains
+        self.assertEqual({'S1C1': ['CS400'],
+                          'S1C2': ['CS404'],
+                          'S1C3': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS404', 'CS412', 'CS416', 'CS420',
+                                   'CS440', 'CS460', 'MATH305', 'ECON401', 'CS490'],
+                          'S2C1': ['CS331', 'CS355', 'CS442'],
+                          'S2C2': ['CS401', 'CS413'],
+                          'S2C3': ['CS335', 'CS415', 'CS419'],
+                          'S3C1': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S3C2': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S3C3': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S4C1': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS401', 'CS404', 'CS413', 'CS415',
+                                   'CS419', 'CS460', 'CS490'],
+                          'S4C2': ['CS420'],
+                          'S4C3': ['CS490']},
+                         domains, "test that should get the correct domain for fall 2008")
+
+    def test_should_return_correct_domain_for_spring_2019(self):
+        filename = "spring_2019.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        domains = csp.domains
+        self.assertEqual({'S1C1': ['CS400'],
+                          'S1C2': ['CS404'],
+                          'S1C3': ['CS327', 'CS331', 'CS335', 'CS355', 'CS400', 'CS401', 'CS404', 'CS413', 'CS415',
+                                   'CS417', 'CS419', 'CS442', 'CS490'],
+                          'S2C1': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S2C2': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S2C3': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S3C1': ['CS331', 'CS345', 'CS460'],
+                          'S3C2': ['CS401', 'CS413'],
+                          'S3C3': ['CS335', 'CS415', 'CS419'],
+                          'S4C1': ['CS327', 'CS331', 'CS335', 'CS355', 'CS400', 'CS404', 'CS408', 'CS411', 'CS412',
+                                   'CS417', 'CS420', 'CS442', 'CS490'],
+                          'S4C2': ['CS420'],
+                          'S4C3': ['CS490']},
+                         domains, "test that should get the correct domain for spring 2019")
+
+    def test_should_return_correct_domain_for_spring_2008(self):
+        filename = "spring_2008.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        domains = csp.domains
+        self.assertEqual({'S1C1': ['CS400'],
+                          'S1C2': ['CS404'],
+                          'S1C3': ['CS327', 'CS331', 'CS335', 'CS355', 'CS400', 'CS404', 'CS408', 'CS411', 'CS412', 'CS417', 'CS420', 'CS442', 'CS490'],
+                          'S2C1': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S2C2': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S2C3': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S3C1': ['CS331', 'CS345', 'CS460'],
+                          'S3C2': ['CS412'],
+                          'S3C3': ['CS335', 'CS416'],
+                          'S4C1': ['CS327', 'CS331', 'CS335', 'CS355', 'CS400', 'CS401', 'CS404', 'CS413', 'CS415', 'CS417', 'CS419', 'CS442', 'CS490'],
+                          'S4C2': ['CS420'],
+                          'S4C3': ['CS490']},
+                         domains, "test that should get the correct domain for spring 2008")
+
+    def test_should_return_correct_domain_for_summer_2019(self):
+        filename = "summer_2019.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        domains = csp.domains
+        self.assertEqual({'S1C1': ['CS400'],
+                          'S1C2': ['CS404'],
+                          'S1C3': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS401', 'CS404', 'CS413', 'CS415', 'CS419', 'CS460', 'CS490'],
+                          'S2C1': ['CS331', 'CS355', 'CS442'],
+                          'S2C2': ['CS411', 'CS412'],
+                          'S2C3': ['CS335'],
+                          'S3C1': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S3C2': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S3C3': ['CS331', 'CS401', 'CS413', 'CS415', 'CS419', 'CS490'],
+                          'S4C1': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS404', 'CS412', 'CS416', 'CS420', 'CS440', 'CS460', 'MATH305', 'ECON401', 'CS490'],
+                          'S4C2': ['CS420'],
+                          'S4C3': ['CS490']},
+                         domains, "test that should get the correct domain for summer 2019")
+
+    def test_should_return_correct_domain_for_summer_2008(self):
+        filename = "summer_2008.txt"
+        student = Student(filename)
+        courses_offered = CoursesOffered(student)
+        csp = CSP(courses_offered)
+        domains = csp.domains
+        self.assertEqual({'S1C1': ['CS400'],
+                          'S1C2': ['CS404'],
+                          'S1C3': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS404', 'CS412', 'CS416', 'CS420', 'CS440', 'CS460', 'MATH305', 'ECON401', 'CS490'],
+                          'S2C1': ['CS331', 'CS355', 'CS442'],
+                          'S2C2': ['CS401', 'CS413'],
+                          'S2C3': ['CS335', 'CS415', 'CS419'],
+                          'S3C1': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S3C2': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S3C3': ['CS331', 'CS412', 'CS420', 'CS440', 'CS490'],
+                          'S4C1': ['CS325', 'CS331', 'CS335', 'CS345', 'CS400', 'CS401', 'CS404', 'CS413', 'CS415', 'CS419', 'CS460', 'CS490'],
+                          'S4C2': ['CS420'],
+                          'S4C3': ['CS490']},
+                         domains, "test that should get the correct domain for summer 2008")
+
+if __name__ == '__main__':
+    unittest.main()
